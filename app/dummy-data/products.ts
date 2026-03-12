2010aff0b5b7ca1f0b17091b4211dd144c80b04d
Christian Kirschberg <ckirschberg@gmail.com>
2026-03-12 14:23:12 +0100

    initial

---
+++ app/dummy-data/products.ts
@@ -0,0 +1,9 @@
+import { Product } from "../../types/product";
+
+export const products: Product[] = [
+    { id: 1, name: "Cat", price: 10, description: "Description 1", category: "Pet" },
+    { id: 2, name: "Dog", price: 5, description: "Description 2", category: "Pet" },
+    { id: 3, name: "Giraffe", price: 120, description: "Description 3", category: "Animal" },
+    { id: 4, name: "Elephant", price: 150, description: "Description 4", category: "Animal" }
+]
+
---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+export function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+                    <img
+                        src="https://bulma.io/images/bulma-logo.png"
+                        alt="Bulma logo"
+                    />
+                </a>
+            </div>
+        </nav>
+    );
+}
