9240eba0a8503057caa6688b06d350be1f53142d
Christian Kirschberg <ckirschberg@gmail.com>
2026-03-23 13:47:24 +0100

    task

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,8 +6,14 @@ import ProductCard from "../../components/ProductCard";
 import { useState } from "react";
 import { Product } from "../../types/product";
 import { log } from "console";
+import Cart from "../../components/Cart";
 
 export default function Products() {
+
+    // Opret en custom hook (se slide)
+    // flyt kode fra denne komponent til useCart (custom hook'en).
+    // Importer de samme funktioner til denne komponent.
+
     const [search, setSearch] = useState("");
     const [cartItemCount, setCartItemCount] = useState(0); // 
     const [cartItems, setCartItems] = useState([] as Product[])
@@ -22,10 +28,13 @@ export default function Products() {
         setCartItemCount(cartItemCount => cartItemCount+1);
         setCartItems([...cartItems, product])
     }
-    console.log(cartItems);
+    const remoteFromCart = () => {
+
+    }
 
     return (
         <div>
+            <Cart cart={cartItems} cartItemCount={cartItemCount}></Cart>
             <h1>Products</h1>
             <h2>Item count in cart: {cartItemCount}</h2>
             <input placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
